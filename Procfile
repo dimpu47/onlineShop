890325f56b986b305bd19750d8cb9cb153fc88c7
Gaurav Choudhary <dimpu47@gmail.com>
2017-09-12 23:37:16 +0530

    initial commit.(grazie universo)

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn onlineShop.wsgi --log-file -
---
+++ cart/cart.py
@@ -0,0 +1,73 @@
+from decimal import Decimal
+from django.conf import settings
+from shop.models import Product
+
+class Cart(object):
+    
+    def __init__(self, request):
+        """
+        Initialize Cart Object.
+        """
+        self.session = request.session
+        cart = self.session.get(settings.CART_SESSION_ID)
+        if not cart:
+            cart = self.session[settings.CART_SESSION_ID] = {}
+        self.cart = cart
+
+    def __len__(self):
+        """
+        Count all items in the cart.
+        """
+        return sum(item['quantity'] for item in self.cart.values())
+
+    def __iter__(self):
+        """
+        Iterate over the items in the cart and get the products from the database.
+        """
+        product_ids = self.cart.keys()
+        # get the product objects and add them to the cart
+        products = Product.objects.filter(id__in=product_ids)
+        for product in products:
+            self.cart[str(product.id)]['product'] = product
+
+        for item in self.cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+    def add(self, product, quantity=1, update_quantity=False):
+        """
+        Add a product to cart or update it's quantity
+        """
+        product_id = str(product.id)
+        if product not in self.cart:
+            self.cart[product_id] = {'quantity': 0,
+                                      'price': str(product.price)}
+        if update_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.save()
+    
+    def save(self):
+        # update the session cart
+        self.session[settings.CART_SESSION_ID] = self.cart
+        # mark the session as "modified" to make sure it is saved
+        self.session.modified = True
+
+    def remove(self, product):
+        """
+        Remove a product from the cart.
+        """
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+    
+    def clear(self):
+        # empty cart
+        self.session[settings.CART_SESSION_ID] = {}
+        self.session.modified = True
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
