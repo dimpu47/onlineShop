8efa20a5336595aab804f85eedeb8efc7f139c80
Gaurav Choudhary <dimpu47@gmail.com>
2017-09-12 23:42:20 +0530

    initial commit.(grazie universo)

--- onlineShop/settings.py
+++ onlineShop/settings.py
@@ -124,11 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT= os.path.join(BASE_DIR, 'static/')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
-)
+
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
